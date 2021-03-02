43a176e35317e12a623694a20a80906313b6f884
sushilnitp <77502277+sushilnitp@users.noreply.github.com>
2021-03-03 00:19:27 +0530

    Initial commit

---
+++ src/main/java/com/uda/demo/controller/DemoRestController.java
@@ -0,0 +1,15 @@
+package com.uda.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello word";
+		
+	}
+	
+}
