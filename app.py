4ba330eee8001d0905a173aa5e47ebc3f9382608
sushilnitp <77502277+sushilnitp@users.noreply.github.com>
2021-03-20 13:49:11 +0530

    commit

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Web App with Python Flask with blue Green deployment!!'
+    return 'Web App with Python Flask with blue Green deployment!!'
 
 app.run(host='0.0.0.0', port=80)
